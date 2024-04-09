eccfacd3ed01f9e9874bd9c900b6aea51705bae2
Vlellw <132191467+Vlellw@users.noreply.github.com>
2024-04-09 20:54:34 +0300

    Update question1_programm.py

--- question1_programm.py
+++ question1_programm.py
@@ -1,10 +1,10 @@
 def insertion_sort(arr):
 	for i in range(1, len(arr)):
-		key = arr[i]
 		j = i-1
-		while j >=0 and key < arr[j]:
+		while j >= 0 and arr[i] < arr[j]:
 			arr[j+1], arr[j] = arr[j], arr[j+1]
 			j -= 1
+			i -= 1
 	return arr
 
 
