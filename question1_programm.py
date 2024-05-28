95802a8f97894be2c487a52832035df50b140ea0
Vlellw <132191467+Vlellw@users.noreply.github.com>
2024-05-28 18:09:13 +0300

    Update question1_programm.py

--- question1_programm.py
+++ question1_programm.py
@@ -100,3 +100,35 @@ bub_sort = bubble_sort(w1)
 sel_sort = selection_sort(w1)
 
 ins_sort = insertion_sort(w1)
+
+
+
+
+
+class Stack:
+
+    def __init__(self):
+        self.stack = []
+        self.max = None
+
+    def pop(self):
+        if len(self.stack) == 0:
+            return None
+        removed = self.stack.pop()
+        if len(self.stack) == 0:
+            self.max = None
+        elif removed == self.max:
+            self.max = self.stack[0]
+            for value in self.stack:
+                if value > self.max:
+                    self.max = value
+        return removed
+
+    def push(self, item):
+        self.stack.append(item)
+        if len(self.stack) == 1 or item > self.max:
+            self.max = item
+
+    def get_max(self):
+        return self.max
+
